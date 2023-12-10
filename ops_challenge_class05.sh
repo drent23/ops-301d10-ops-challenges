6b63346b51440e03ef1185c25c7b3820975d60a6
drent23 <david.j.renteria1@gmail.com>
2023-12-10 11:28:50 -0600

    revised

--- ops_challenge_class05.sh
+++ ops_challenge_class05.sh
@@ -1,35 +1,44 @@
 !#/bin/bash
 
-
-
-# Create a function for script
-compressing_log_files() {
-# Assign variable to log files
+# Assign log files to an array/list
 log_files=("/var/log/syslog" "/var/log/wtmp")
 
-# Create a backup directory
-backup_directory="/var/log/backups/syslog"
-sudo mkdir -p "$backup_directory"
+# Declare backup directory
+backup_directory="/var/log/backups"
 
-# Create timestamp using YYYYMMDDHHSS
-timestamp=$(date "+%Y%m%d%H%M%S")
-# Print to screen the size of each file
-    for file in "${log_files[@]}"; do
-        # Get original size
-        og_size=sudo $(du -h "${file}" | awk '{print $1}')
-        echo "The original size of $file is $og_size."
-        # Compress log files to backup directory and add timestamp
-        comp_file="sudo ${backup_directory}/$(basename ${file})-${timestamp}.gzip"
-        sudo gzip -c "${file}" > "${comp_file}"
-        # Clear contents of the log file
-        > "$file"
-        # Get compressed size
-        comp_size=$(du -h "${comp_file}" | awk '{print $1}')
-        echo "The compressed size of $comp_file is $comp_size."
-        # Compare sizes
-        echo "The difference between the original size $og_size and the compressed size $comp_size is ($og_size - $comp_size)."
-    done
+# Create a backup directory
+mkdir -p "$backup_directory"
+
+# Create timestamp
+timestamp=$(date "+%Y%m%d-%H:%M:%S")
+
+# Declare a function to compress files
+compress_files() {
+    file="$1"
+    # Create compressed file
+    comp_file="$backup_directory/$(basename $file)-$timestamp.gzip"
+    # Get original size
+    og_size=$(stat -c %s "$file")
+    echo "The original size of $file is $og_size."
+    # Compress log files to backup directory and add timestamp
+    gzip -c "$file" > "$comp_file"
+    # Get compressed size
+    comp_size=$(stat -c %s "$comp_file")
+    echo "The compressed size of $comp_file is $comp_size."
+    # Compare sizes
+    echo "Original size: $og_size vs Compressed size: $comp_size."
+    # Clear contents of the log file
+    > "$file"
+
 # Use 'shred' to clear tracks of who logged into the system to manipulate/compress log files
-shred -vfzu auth.log
+    shred -vfzu auth.log
 }
-compressing_log_files
+# Go through log files and backup/compress
+for file in "${log_files[@]}"; do
+    if [ -e "$file" ]; then
+        compress_files "$file"
+        echo "$file backedup and compressed."
+    else
+        echo "That file doesn't exist."
+    fi 
+done

95493cdb0d229cf9e569e8e721a70730db49cf63
drent23 <david.j.renteria1@gmail.com>
2023-12-17 10:38:51 -0600

    added pw to user

--- ops_challenge_class13.ps1
+++ ops_challenge_class13.ps1
@@ -2,7 +2,7 @@
 Import-Module ActiveDirectory
 
 # Create new AD user 'Franz Ferdinand'
-New-ADUser -SamAccountName "fferdinand" -Name "FranzFerdinand" -OtherAttributes @{'city'="Springfield";'department'="TPS";'mail'+"ferdi@GlobeXpower.com";'state'="OR";'title'="TPS Reporting Lead"}
+New-ADUser -SamAccountName "fferdinand" -Name "FranzFerdinand" -AccountPassword (ConvertTo-SecureString -AsPlainText 'P@$$W0rd' -Force) Enabled = $true -OtherAttributes @{'city'="Springfield";'department'="TPS";'mail'+"ferdi@GlobeXpower.com";'state'="OR";'title'="TPS Reporting Lead"}
 
 # Create new Group
 New-ADGroup -SamAccountName ReportingLeads =Name "Reporting Leads" -Description "Reporting Leads for TPS Department" -DisplayName "Reporting Leads" -GroupCategory 1 -GroupScope 1 -DisplayName "TPS Reporting Leads"
